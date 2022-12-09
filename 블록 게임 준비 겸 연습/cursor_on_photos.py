91f016294ed71e9ec9ce1aa0ab3c3b114d1d2ea1
우성원 <frosttvinkorea@gmail.com>
2022-12-09 12:28:31 +0900

    블록게임 준비 겸 연습
    블록 낙하 게임 만들기 실습에 필요한 요소들을 만들며 연습을 한 결과물들 입니다. Motion과 2차원 리스트 등을 이용하여 커서 표시를 하고, 캔버스에 사진을 격자 안에 알맞게 집어넣은 부분을 연습하였습니다.

---
+++ 블록 게임 준비 겸 연습/cursor_on_photos.py
@@ -0,0 +1,66 @@
+import tkinter
+
+cursor_x = 0
+cursor_y = 0
+mouse_x = 25
+mouse_y = 25
+
+def mouse_move(e):
+    global mouse_x, mouse_y
+    mouse_x = e.x
+    mouse_y = e.y
+
+def game_main():
+    global cursor_x, cursor_y
+    if 24 <= mouse_x and mouse_x < 24 + 72 * 8 and 24 <= mouse_y and mouse_y < 24 + 72 * 10:
+        cursor_x = int((mouse_x - 24) / 72)
+        cursor_y = int((mouse_y - 24) / 72)
+        cvs.delete("CURSOR")
+        cvs.create_image(cursor_x*72 + 60, cursor_y*72 + 60, image=cursor, tag="CURSOR")
+        root.after(1, game_main)
+    else:
+        root.after(1, game_main)
+
+neko = [
+    [7, 7, 0, 0, 0, 0, 7, 7],
+    [7, 7, 0, 0, 2, 0, 7, 7],
+    [0, 0, 3, 0, 2, 1, 0, 0],
+    [0, 0, 4, 3, 2, 1, 0, 0],
+    [0, 5, 4, 3, 2, 1, 0, 0],
+    [0, 5, 4, 3, 0, 6, 0, 0],
+    [0, 5, 4, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [7, 7, 0, 0, 0, 0, 7, 7],
+    [7, 7, 1, 2, 3, 4, 7, 7]
+    ]
+
+def draw_neko():
+    for y in range(10):
+        for x in range(8):
+            if neko[y][x] > 0:
+                cvs.create_image(x * 72 + 60, y * 72 + 60, image=img_neko[neko[y][x]])
+
+
+root = tkinter.Tk()
+root.title("배치한 그림 위 커서 표시")
+root.resizable(False, False)
+cvs = tkinter.Canvas(root, width=912, height=768)
+cvs.pack()
+root.bind("<Motion>",mouse_move)
+bg = tkinter.PhotoImage(file = "neko_bg.png")
+cursor = tkinter.PhotoImage(file="neko_cursor.png")
+img_neko = [
+    None,
+    tkinter.PhotoImage(file="neko1.png"),
+    tkinter.PhotoImage(file="neko2.png"),
+    tkinter.PhotoImage(file="neko3.png"),
+    tkinter.PhotoImage(file="neko4.png"),
+    tkinter.PhotoImage(file="neko5.png"),
+    tkinter.PhotoImage(file="neko6.png"),
+    tkinter.PhotoImage(file="neko_niku.png")
+    ]
+
+cvs.create_image(456, 384, image=bg)
+draw_neko()
+game_main()
+root.mainloop
